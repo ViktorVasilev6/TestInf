a602ecbaf12019e325c2f3ff1321d8a5459959be
DontMindMyNamePls <viktor_vv@abv.bg>
2021-02-07 09:21:18 +0200

    Add firstproj

---
+++ FirstProgram/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Inches: ");
+            var inches = double.Parse(Console.ReadLine());
+            var cm = inches * 2.56;
+            Console.WriteLine("Centimeters: " + cm);
+        }
+    }
+}
